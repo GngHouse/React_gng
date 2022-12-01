83f8513687a7c268d8302824510e7bc42b2334a9
민석 <80371412+MinSeok0123@users.noreply.github.com>
2022-12-01 11:06:07 +0900

    sample

--- gnghouse/src/components/First.js
+++ gnghouse/src/components/First.js
@@ -5,7 +5,7 @@ export default function First() {
   return (
     <div>
       <img
-        src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ftistory1.daumcdn.net%2Ftistory%2F5019948%2Fattach%2F5167564a97eb43329238a34c6e51a191"
+        src="/img/sample.png"
         class="hand"
       ></img>
     </div>
