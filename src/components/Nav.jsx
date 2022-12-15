722e8c1861c701e151fbbb40ce243da2e7db5244
민석 <80371412+MinSeok0123@users.noreply.github.com>
2022-12-15 10:46:36 +0900

    Fix :: Arrow in Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,19 +5,19 @@ export function Nav() {
     <div className="leftnav">
       <a href="#sectionOne">
         <div className="Nav_top">
-          <span className="맨위">맨 위</span>
+          {/* <span className="맨위">맨 위</span> */}
           <img className="up" src="/img/arrow.png"></img>
         </div>
       </a>
       <a href="#sectionSix">
         <div className="Nav_middle">
-          <span className="nav공간연출">공간연출</span>
+          {/* <span className="nav공간연출">공간연출</span> */}
           <img className="middle" src="/img/원.png"></img>
         </div>
       </a>
       <a href="#sectionEleven">
         <div className="Nav_bottom">
-          <span className="맨밑">맨 밑</span>
+          {/* <span className="맨밑">맨 밑</span> */}
           <img className="down" src="/img/arrow.png"></img>
         </div>
       </a>
