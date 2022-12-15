3aefefa1c13434f16e2fb77a95135ba84ad18334
민석 <80371412+MinSeok0123@users.noreply.github.com>
2022-12-15 10:34:15 +0900

    FIx :: Production

--- src/components/Production.jsx
+++ src/components/Production.jsx
@@ -3,9 +3,11 @@ import "./Production.css";
 export function Production() {
   return (
     <div className="production">
-      <img className="pro_bg" src="/img/Pro_bg1.jpeg"></img>
+      <img className="pro_bg" src="/img/Pro_bg2.png"></img>
+      <span className="pro_gng">GIVE & GIVE</span>
+      <span className="pro_space">SPACE PRODUCTION</span>
       <div className="procenter">
-        <span className="pro_bottom">공&nbsp;간&nbsp;연&nbsp;출</span>
+        <span className="pro_bottom">공간&nbsp;연출</span>
       </div>
     </div>
   );
